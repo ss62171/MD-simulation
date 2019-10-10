65a051753af1b7364c52fc2726839ff739327e21
Shubham Sharma <shubham.s@ada.iiit.ac.in>
2019-10-10 05:56:16 +0530

    with villin

--- simulateSteered.py
+++ simulateSteered.py
@@ -7,18 +7,20 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-pdb = PDBFile('villin.pdb')
+pdb = PDBFile('1tki.pdb')
 spring_constant=0.00050
 pullto=np.zeros(3)
 extension = []
 t = []
-spring_k = spring_constant * u.kilocalorie / (u.mole * u.nanometer * u.nanometer)
-
+spring_k = spring_constant * u.kilocalorie / (u.mole * u.angstrom * u.angstrom)
 forcefield = ForceField('amber99sb.xml', 'amber99_obc.xml')
-system = forcefield.createSystem(pdb.topology, nonbondedMethod=NoCutoff, nonbondedCutoff=1*nanometer)
+system = forcefield.createSystem(pdb.topology, nonbondedMethod=NoCutoff, nonbondedCutoff=1*angstrom)
+
 integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
+
 simulation = Simulation(pdb.topology, system, integrator)
 simulation.context.setPositions(pdb.positions)
+
 simulation.minimizeEnergy()
 
 pullforce = CustomExternalForce('spring_k*(dx^2+dy^2+dz^2); \
@@ -29,7 +31,7 @@ pullforce.addPerParticleParameter("x0")
 pullforce.addPerParticleParameter("y0")
 pullforce.addPerParticleParameter("z0")
 pullforce.addGlobalParameter("spring_k", spring_k)
-pullforce.addParticle(595, [pullto[0], pullto[1], pullto[2]])
+pullforce.addParticle(5737, [pullto[0], pullto[1], pullto[2]])
 
 system = simulation.context.getSystem()
 system.addForce(pullforce)
@@ -37,27 +39,28 @@ currentState = simulation.context.getState()
 simulation.context.reinitialize()
 simulation.context.setState(currentState)
 currentContext = simulation.context
-vel = np.zeros((596,3))
-vel = (vel * u.nanometer)/u.picosecond
+vel = np.zeros((5738,3))
+vel = (vel * u.angstrom)/u.picosecond
 currentContext.setVelocities(vel)
 system.setParticleMass(0,0)
 
 simulation.context.setPositions(pdb.positions)
 
 pos = simulation.context.getState(getPositions=True).getPositions()
-b0_pos = np.array([pos[595]/u.nanometer])
-a0_pos = np.array([pos[0]/u.nanometer])
+b0_pos = np.array([pos[5737]/u.angstrom])
+a0_pos = np.array([pos[0]/u.angstrom])
 b_t = np.array([])
 unit_vector = np.array([])
 dist_init = np.linalg.norm(b0_pos-a0_pos)
 F = []
+W = np.array([])
 
-for i in range(200000):
+for i in range(2000):
         # Get current state tu update the system
         pos = simulation.context.getState(getPositions=True).getPositions()   
-        alpha = 1 + 0.002*i 
-        b_t = pos[595]/u.nanometer
-        a_t = pos[0]/u.nanometer
+        alpha = 1 + 0.02*i 
+        b_t = pos[5737]/u.angstrom
+        a_t = pos[0]/u.angstrom
         f_t = 1 + ((b0_pos-a0_pos)/dist_init)*alpha
         f_t = np.squeeze(f_t)
         dist_now = np.linalg.norm(b_t-f_t)
@@ -66,23 +69,24 @@ for i in range(200000):
         stdout.write('\r{}'.format(i))
         a = (dist_now - 1)/dist_now
         force = spring_constant * a * (b_t - f_t)
-        F.append(np.linalg.norm(force))   
+        F.append(np.linalg.norm(force))
+        # w = spring_constant*0.02*0.5(0.02*i*i - )
         
-        unit_vector = np.array([pos[595]/u.nanometer - pos[0]/u.nanometer])
+        unit_vector = np.array([pos[5737]/u.angstrom - pos[0]/u.angstrom])
         unit_vector = np.squeeze(unit_vector)
-        dist = np.linalg.norm((pos[595]/u.nanometer)-(pos[0]/u.nanometer))
+        dist = np.linalg.norm((pos[5737]/u.angstrom)-(pos[0]/u.angstrom))
         unit_vector = unit_vector/dist
         extension.append(dist)
         t.append(i)
-        pullto[0] = (pos[595][0]/u.nanometer)+0.05*(i+1)*unit_vector[0]
-        pullto[1] = (pos[595][1]/u.nanometer)+0.05*(i+1)*unit_vector[1]
-        pullto[2] = (pos[595][2]/u.nanometer)+0.05*(i+1)*unit_vector[2]
+        pullto[0] = (pos[5737][0]/u.angstrom)+0.8*(i+1)*unit_vector[0]
+        pullto[1] = (pos[5737][1]/u.angstrom)+0.8*(i+1)*unit_vector[1]
+        pullto[2] = (pos[5737][2]/u.angstrom)+0.8*(i+1)*unit_vector[2]
         pullforce.setParticleParameters(0,595,[pullto[0],pullto[1],pullto[2] ])
         pullforce.updateParametersInContext(currentContext)
-        # curr_vel = simulation.context.getState(getVelocities=True).getVelocities()
-        # curr_vel[0] = 0*(curr_vel[0])
-        # currContext = simulation.context
-        # currContext.setVelocities(curr_vel)
+        curr_vel = simulation.context.getState(getVelocities=True).getVelocities()
+        curr_vel[0] = 0*(curr_vel[0])
+        currContext = simulation.context
+        currContext.setVelocities(curr_vel)
         # stdout.write('\r{0}'.format(simulation.context.getState(getVelocities=True).getVelocities()[0]))
         simulation.step(1)
 
@@ -92,4 +96,7 @@ plt.plot(extension,F)
 plt.xlabel('Extension')
 plt.ylabel('Force')
 plt.show()
+plt.plot(t,extension)
+plt.show()
+
 
