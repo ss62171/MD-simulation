37112eacbe467af29204f97f02ec531a970be16d
Shubham Sharma <shubham.s@ada.iiit.ac.in>
2019-10-28 23:56:10 +0530

    batch job submission script

--- simulateSteered.sh
+++ simulateSteered.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A research
-#SBATCH -c 10
-#SBATCH --gres=gpu:2
+#SBATCH -c 4
+#SBATCH --gres=gpu:1
 #SBATCH --mem-per-cpu=3000
 #SBATCH --time=4-00:00:00
 #SBATCH --mail-type=END
